c26ebd7f34a6b3de00979593f2a46bf04f88303d
EnKayAre <nkrohrlich@gmail.com>
2020-08-01 14:00:43 -0400

    Removed bot key for security

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const API_KEY = '';
+
 const Discord = require('discord.js');
  const client = new Discord.Client();
 
@@ -53,4 +55,4 @@ client.on('message', msg => {
 }
 });
 
-client.login('NzM4NzgxNzk0MjMyNTY1ODAw.XyQ6KQ.z4wQuw9NSNncFuPrE1YGGgIYB6o');
+client.login(API_KEY);
